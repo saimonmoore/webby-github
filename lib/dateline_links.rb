268b7439b263a026e210656e05433cd4a6b481f2
Mike Green <mike.is.green@gmail.com>
2009-01-22 00:13:11 -0500

    created ATOM feed template, and a basic blog post list on homepage still need to complete navigation between posts, and find out how locomotivation.com gets their Disqus comments to show up in-page

---
+++ lib/dateline_links.rb
@@ -0,0 +1,27 @@
+module DatelineLinksHelper
+	
+	def dateline_links(page)
+		time					= page.created_at
+		day_number		= time.strftime("%d")
+		day_name			= time.strftime("%A")
+		month_number	= time.strftime("%m")
+		month_name		= time.strftime("%B")
+		year					= time.strftime("%Y")
+		
+		html = %(<span class="dateline">\n#{day_name}, <a href="blog/#{year}/#{month_number}/index.html">#{month_name}</a>)
+		html += %( #{day_number}, <a href="blog/#{year}/index.html">#{year}</a></span>)
+		html
+	end
+	
+	def dateline(page)
+		time = page.created_at
+		day = time.strftime("%d")
+		month = time.strftime("%B")
+		year = time.strftime("%Y")
+		html = %(<span class="dateline">#{month} #{day}, #{year} :: </span>)
+		html
+	end
+	
+end
+
+Webby::Helpers.register(DatelineLinksHelper)
